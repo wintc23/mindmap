b350098aca808f07bc61d467ba91bc127d287337
Lushenggang <lushg-tcxg@qq.com>
2018-12-07 18:13:11 +0800

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,16 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="root">
+    <div class="map-tree">
+      <Button type="success" @click.stop="calNodePosition()">渲染</Button>
+      <simple-tree
+        class="tree"
+        :treeData="treeData">
+        <div slot-scope="{data, parentData, level}">
+          {{ data.name }}
+        </div>
+      </simple-tree>
+    </div>
+    <div class="mindmap" ref="mindmap"></div>
   </div>
 </template>
 
@@ -35,24 +19,102 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data () {
+    return {
+      treeData: [
+        {
+          name: 'hello',
+          id: '1',
+          children: [
+            {
+              name: 'child1',
+              id: '2'
+            }
+          ]
+        }
+      ]
+    }
+  },
+  methods: {
+    createCurve () {
+      // 
+    },
+    calNodePosition () {
+      this.createElements(this.treeData)
+      this.calBoxPosition(this.treeData)
+    },
+    createElements (treeData) {
+      for (let data of treeData) {
+        this.createElement(data)
+        if (data.children) {
+          this.createElements(data.children)
+        }
+      }
+    },
+    createElement (data) {
+      let element = data.element
+      if (!element) {
+        element = document.createElement('div')
+      }
+      element.classList.add('mind-map-node')
+      element.innerText = data.name
+      this.$refs.mindmap.appendChild(element)
+      let rect = element.getBoundingClientRect()
+      data.width = rect.width
+      data.height = rect.height
+      data.element = element
+    },
+    calBoxPosition (treeData) {
+      for (let data of treeData) {
+        if (data.children) {
+          this.calBoxPosition(data.children)
+          let height = 0
+          let width = 0
+          data.children.forEach(item => {
+            width = Math.max(item.width, width)
+            height = item.height + 16
+          })
+        } else {
+          data.boxWidth = data.width
+          data.boxHeight = data.height
+        }
+      }
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style lang="stylus" scoped>
+.root
+  position fixed
+  top 0
+  left 0
+  bottom 0
+  right 0
+  display flex
+  padding 1rem
+  .mindmap
+    box-shadow 0 0 1px 0 #3361D8
+    position relative
+    flex auto
+    margin-left 1rem
+    border-radius 4px
+  .map-tree
+    flex auto
+    box-shadow 0 0 1px 0 #3361D8
+    border-radius 4px
+    display flex
+    flex-direction column
+    .tree
+      flex auto
+</style>
+
+<style lang="stylus">
+.mind-map-node
+  position absolute
+  box-shadow 0 0 1px 1px #3361D8
+  padding 4px .5rem
+  border-radius 2px
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import simpleTree from 'simple-vue-tree'
+import iView from 'iview'
+import 'simple-vue-tree/dist/lib/simple-tree.css'
+import 'iview/dist/styles/iview.css'
 
+Vue.use(iView)
+Vue.use(simpleTree)
 Vue.config.productionTip = false
 
 new Vue({
